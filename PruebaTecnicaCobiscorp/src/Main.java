d112573eb153576149f920559574e6c12552ded9
Arnaldo Barrios <abarrime@everis.com>
2020-06-05 15:20:37 -0500

    Primera Subida del proyecto.

---
+++ PruebaTecnicaCobiscorp/src/Main.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author abarrime
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+         int k = 3, m = 2;
+          
+          System.out.println(!(k > m));
+          
+    }
+    
+    
+
+}
---
+++ PruebaTecnicaCobiscorp/src/Q2.java
@@ -0,0 +1,22 @@
+
+import java.awt.Button;
+import java.awt.Frame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author abarrime
+ */
+public class Q2 extends Frame {
+
+    Q2() {
+        setSize(300, 300);
+        Button button = new Button("Aceptar");
+        add(button);
+    }
+
+}
